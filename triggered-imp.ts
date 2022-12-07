6fec0eec48074cf587fa967f79dd6ff98714a97a
sci-barite <romo.mulas@gmail.com>
2022-12-07 18:20:55 +0200

    Lancer 1.3.2 (Adding logic to try and outsource the indexing of FWDB to Lancer via time triggers)

--- triggered-imp.ts
+++ triggered-imp.ts
@@ -1,4 +1,6 @@
 function imp() {
     tickerUpdate('','⚠️');
     dbFy(false, 'LeadsDB');
+    UrlFetchApp.fetch(getFWDBPost(), 
+        {method: 'post', contentType: 'text/plain', payload: 'NewUniqueJobs:'+PropertiesService.getScriptProperties().getProperty('NewUniqueJobs')});
 }
