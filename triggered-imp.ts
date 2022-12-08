9724ae9fc8bb6b3cfdd0da37492b405f5ad0bd38
sci-barite <romo.mulas@gmail.com>
2022-12-08 12:40:27 +0200

    Lancer 1.3.3 (Preparing for Sylph integration of new IDs)

--- triggered-imp.ts
+++ triggered-imp.ts
@@ -12,11 +12,15 @@ function imp() {
             UrlFetchApp.fetch(getFWDBPost(), 
                 {method: post, contentType: textPlain, payload: prop+sub+':'+PropertiesService.getScriptProperties().getProperty(prop+sub)});
             if (sub === '') {
-                const Structured : {[key: string]: any}[] = JSON.parse(PropertiesService.getScriptProperties().getProperty(prop) as string);
-                const IDsOnly = Structured.map(entry => entry['ID']);
-                PropertiesService.getScriptProperties().setProperty(prop.substring(3), JSON.stringify(IDsOnly));
+                prepareForSylph(prop);
             }
         })
         Utilities.sleep(2000);
     })
+}
+
+function prepareForSylph(prop: string) {
+    const Structured : {[key: string]: any}[] = JSON.parse(PropertiesService.getScriptProperties().getProperty(prop) as string);
+    const IDsOnly = Structured.map(entry => entry['ID']);
+    PropertiesService.getScriptProperties().setProperty(prop.substring(3), JSON.stringify(IDsOnly));
 }
