34fc3f0d81b230fa3a963ac40ab26d2ae9017fe7
sci-barite <romo.mulas@gmail.com>
2022-12-08 10:01:42 +0200

    Lancer 1.3.2 (Triggered Imp)

--- triggered-imp.ts
+++ triggered-imp.ts
@@ -3,4 +3,10 @@ function imp() {
     dbFy(false, 'LeadsDB');
     UrlFetchApp.fetch(getFWDBPost(), 
         {method: 'post', contentType: 'text/plain', payload: 'NewUniqueJobs:'+PropertiesService.getScriptProperties().getProperty('NewUniqueJobs')});
+    Utilities.sleep(2000);
+    UrlFetchApp.fetch(getFWDBPost(), 
+        {method: 'post', contentType: 'text/plain', payload: 'NewUniqueJobs.last:'+PropertiesService.getScriptProperties().getProperty('NewUniqueJobs.last')});
+    Utilities.sleep(2000);
+    UrlFetchApp.fetch(getFWDBPost(), 
+        {method: 'post', contentType: 'text/plain', payload: 'NewUniqueJobs.doubles:'+PropertiesService.getScriptProperties().getProperty('NewUniqueJobs.doubles')});
 }
