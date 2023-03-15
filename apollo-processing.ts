7dce4dfb22942d4140a07945cb68ea4e57e93a2b
sci-barite <romo.mulas@gmail.com>
2023-03-15 18:45:32 +0200

    Lancer 1.3.4 (Hopefully last major refactoring AGAIN, towards final and universal version and API)

--- apollo-processing.ts
+++ apollo-processing.ts
@@ -16,7 +16,6 @@ type ApolloContact = {
 
 type DBContact = Partial<ApolloContact> & {
     Row: number,
-    Date: string,
     Comment: string,
     Name_id: string,
     Company_id: string,
@@ -27,12 +26,12 @@ function Get(...args : string[]) : {[key: string]: any} {
     const Pack : {[key: string]: any} = {};
     Pack.Today = new Date().toLocaleDateString();
     if (args.includes('ContactsDB')) {
-        Pack.Cols = {
-            Row: 0, Name: 1, Name_id: 2, Status: 3, Title: 4, Location: 5, Company: 6, Employees: 7, 
+        Pack.Cols = {   // Column numbers starting from zero, to be adjusted by 1 when using the getRange method.
+            Checkbox: 0, Name: 1, Name_id: 2, Status: 3, Title: 4, Location: 5, Company: 6, Employees: 7, 
             Company_id: 8, Date: 9, Comment: 10, Phone: 11, Company_web: 12, Email: 13
         };
         Pack.Default = {
-            Status: '0.Imported', Date: Pack.Today,
+            Status: '0.Imported', Date: Pack.Today, Checkbox: true,
             NewMessage: 'Imported via Sylph!', OldMessage: 'Updated via Sylph on '+Pack.Today, URL: 'https://app.apollo.io/',
         };
         Pack.DB = SpreadsheetApp.openById(getFWDBLeads()).getSheetByName('ContactsDB');
@@ -57,63 +56,61 @@ function ContactsList(Contacts: ApolloContact[]) {
     const UpdRows : number[] = [];
 
     Contacts.forEach(Contact => {
-        const ExRow = get.Names.findIndex((name: string) => name == Contact.Name) + 1;
-        const Jobs = Contact.Jobs? buildJobsString(Contact.Jobs, Contact.More).split('!')[1] : '';
+        const ExRow = get.Names.findIndex((name: string) => name == Contact.Name) + 1;  // Might be better to use uniqueIDs at some point.
+        const Jobs = Contact.Jobs? buildJobsString(Contact.Jobs, Contact.More).split('!')[1] : '';  // Maybe Sylph should do this instead.
         const Entry : DBContact = {
             ...get.Default,
             ...Contact as ApolloContact,
             Row: ExRow,
-            Comment: `${ExRow ? '' : get.Default.NewMessage}${Jobs}${ExRow ? get.Default.OldMessage : ''}`,
-            Apollo_link: `${Contact.Name_apollo.startsWith('#') ? get.Default.URL : ''}${Contact.Name_apollo}`,
+            Comment: `${ExRow ? '' : get.Default.NewMessage}${Jobs}${ExRow ? '\n'+get.Default.OldMessage : ''}`,
             Name_id: !Contact.Name_linkedin ? '' : Contact.Name_linkedin.split('/in/')[1].replace('/', ''),
-            Company_id: !Contact.Company_linkedin ? '' : Contact.Company_linkedin.split('/company/')[1].replace('/', '')
+            Company_id: !Contact.Company_linkedin ? '' : Contact.Company_linkedin.split('/company/')[1].replace('/', ''),
+            Apollo_link: `${Contact.Name_apollo.startsWith('#') ? get.Default.URL : ''}${Contact.Name_apollo}`
         };
         Entry.Row ? UpdContacts.push(Entry) : NewContacts.push(Entry);
     });
 
     const writeRow = (DBContact: DBContact) => Object.keys(get.Cols).map((col) => DBContact[col as keyof DBContact] ?? '');
+    const updateRows = (Values: any[][], Range: GoogleAppsScript.Spreadsheet.Range, Index: number[], FirstRow: number, Rich?: string) =>
+        (Rich ? Range.getRichTextValues() : Range.getValues()).map((Cols, RowN) => {
+            const Row = Index.indexOf(FirstRow + RowN);
+            if (Row === -1) return Cols;
+            if (!Rich) Values[Row][get.Cols.Comment] = `${Cols[get.Cols.Comment]}\n${Values[Row][get.Cols.Comment]}`;  // Fastest way, I think.
+            return Values[Row];
+        });
 
-    [NewContacts, UpdContacts].forEach((DataLoad, Upd) => {
-        const Data = DataLoad.length
-        if (!Data) return;
-        const Rows = Upd ? DataLoad.map(Contact => Contact.Row) : [];
-        if (Upd) UpdRows.push(...Rows.sort((a, b) => a - b));
-        const FirstRow = Upd ? UpdRows[0] : get.NewRow, Quantity = Upd ? (UpdRows[Rows.length - 1] - FirstRow) + 1 : Data;
-        const Values = DataLoad.map(contact => writeRow(contact));
-        if (!Upd) get.DB.insertRowsAfter(get.NewRow, Data);
-        const ValueRange = get.DB.getRange(FirstRow, 1, Quantity, Values[0].length);
-        const OldValues : string[][] = Upd ? ValueRange.getValues() : [[]]
-        const UpdValues = Upd ? OldValues.map((Cols, RowN) => {
-            const Row = Rows.indexOf(FirstRow + RowN);
-            if (Row >= 0) return Cols.map((Field, Col) => Col != get.Cols.Comment ? Values[Row][Col] : `${Field}\n${Values[Row][Col]}`);
-            else return Cols;
-        }) : [[]];
-
+    [NewContacts, UpdContacts].forEach((Contacts, Upd) => {
+        // Setup phase. We want to know how much we have in terms of rows. For updates, we want everything from first row to last.
+        const DataLength = Contacts.length
+        if (!DataLength) return;
+        const RowIndex = Upd ? Contacts.map(Contact => Contact.Row) : [];
+        if (Upd) UpdRows.push(...RowIndex.sort((a, b) => a - b));   // Sorting to get the smallest value, and report to Sylph.
+        const FirstRow = Upd ? UpdRows[0] : get.NewRow, Rows = Upd ? (UpdRows[UpdRows.length - 1] - FirstRow) + 1 : DataLength;
+        // Regular values. Here the main difficulty is updating: rows are found via indexOf on Rows, which has the same order as the source.
+        const Values = Contacts.map(Contact => writeRow(Contact)), Columns = Values[0].length;
+        if (!Upd) get.DB.insertRowsAfter(get.NewRow, DataLength);   // We insert new rows before so we never miss with getRange, below.
+        const ValueRange = get.DB.getRange(FirstRow, get.Cols.Checkbox + 1, Rows, Columns);   // Avoiding magic numbers as much as possible.
+        // RichText values. We build them all first, then pick if updating. Link in Comment was moved to Title to reduce to a single range.
         const Riches = Values.map((Cols, Row) => 
-            Cols.slice(1, 7).map((Field, Col) => RichCols.includes(Col) ? SpreadsheetApp.newRichTextValue()
-                .setText(Field as string).setLinkUrl(DataLoad[Row][Link[RichCols.indexOf(Col)] as keyof DBContact] as string).build()
-                : SpreadsheetApp.newRichTextValue().setText(Field as string).build()
+            Cols.slice(get.Cols.Name, get.Cols.Company + 1).map((Field, Col) => 
+                RichCols.includes(Col) ? SpreadsheetApp.newRichTextValue()
+                    .setText(Field as string).setLinkUrl(Contacts[Row][Link[RichCols.indexOf(Col)] as keyof DBContact] as string).build()
+                    : SpreadsheetApp.newRichTextValue().setText(Field as string).build()
             )
         );
-        const RichRange = get.DB.getRange(FirstRow, 2, Quantity, 6);
-        const OldRiches : GoogleAppsScript.Spreadsheet.RichTextValue[][] = Upd? RichRange.getRichTextValues() : [[]];
-        const UpdRiches = Upd ? OldRiches.map((Cols, RowN) => {
-            const Row = Rows.indexOf(FirstRow + RowN)
-            if (Row >= 0) return Riches[Row];
-            else return Cols;
-        }) : [[]];
+        const RichRange = get.DB.getRange(FirstRow, get.Cols.Name + 1, Rows, get.Cols.Company);
         
-        ValueRange.setValues(Upd ? UpdValues.map(Cols => [true, ...Cols.slice(1)]) : Values.map(Cols => [true, ...Cols.slice(1)]));
-        RichRange.setRichTextValues(Upd ? UpdRiches : Riches);
-        if (!Upd) get.DB!.getRange(get.NewRow, 1, Quantity, 1).insertCheckboxes().check();
-    })
+        ValueRange.setValues(Upd ? updateRows(Values, ValueRange, RowIndex, FirstRow) : Values);
+        RichRange.setRichTextValues(Upd ? updateRows(Riches, RichRange, RowIndex, FirstRow, 'Rich') : Riches);
+        if (!Upd) get.DB!.getRange(get.NewRow, get.Cols.Checkbox + 1, Rows, get.Cols.Checkbox + 1).insertCheckboxes().check();
+    });
 
     const [NewRows, Upd] = [NewContacts.length, UpdRows.length];
     const Resp = {Row: get.DB.getLastRow() as number, Added: NewRows, Update: Upd ? true : false, Updated: '', Message: ''};
     Resp.Updated = Upd ? `${UpdRows[0]}` : '';
     const Msg = `Last row ${Resp.Row}: ${JSON.stringify(get.DB.getRange(`${Resp.Row}:${Resp.Row}`).getValues())}`;
-    const Update = Upd ? `${UpdRows.length} updated! Row(s): ${UpdRows.toString()}`: (NewRows ? '' : `\nNo updates.`);
-    Resp.Message = `${Msg}\n🧜‍♂️ Lancer has added ${NewRows} new contact${NewRows == 1 ? '' : 's'}!\n${Update}`;
+    const Update = Upd ? `${Upd} updated! Row(s): ${UpdRows.toString()}`: (NewRows ? '' : `\nNo updates.`);
+    Resp.Message = `${Msg}\n🧜‍♂️ Lancer has added ${NewRows} new contact(s)!\n${Update}`;
 
     const JSONOutput = ContentService.createTextOutput(JSON.stringify(Resp));
     JSONOutput.setMimeType(ContentService.MimeType.JSON);
