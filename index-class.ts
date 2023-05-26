2b2901bd00ab0f8855dba4e1055196edf591f581
sci-barite <romo.mulas@gmail.com>
2023-05-26 12:41:43 +0300

    Lancer 1.4.1 (New, hopefully final Index class/IIFE, added addElems, deleteId, and fixed/changed several things here and there to get consistent behavior)

--- index-class.ts
+++ index-class.ts
@@ -3,8 +3,8 @@ type ColumnInfo = {A1: string, sheetName: string, short?: boolean, doubles?: boo
 type IndexInfo = {name: string, spreadsheet: string, stores?: number};
 
 const Index = (() => {
-    const [sheetAddress, colAddress] = [1, 2];
-    const object : {[sheet: string]: {[column: string]: ColumnIndex}} = {};
+    const [sheetAddress, colAddress, excludedStoreTags] = [1, 2, ['.index', '.info']];
+    const objMod : {[sheet: string]: {[column: string]: ColumnIndex}} = {};
     const caches : WeakMap<ColumnIndex, IndexProps> = new WeakMap();
     const colMap : Map<string, string[]> = new Map();
     const sheets : Set<string> = new Set();
@@ -23,12 +23,15 @@ const Index = (() => {
             return this;
         }
         public getCache = () => caches.get(this);
-        public readProp = () => JSON.parse(params.stores.getProperty(this.#key + '.index')!) as IndexProps;
-        public getProps = () => caches.set(this, JSON.parse(params.stores.getProperty(this.#key + '.index')!) as IndexProps);
+        public getProps = () => JSON.parse(params.stores.getProperty(this.#key + '.index')!) as IndexProps;
+        public readProp = () => {
+            caches.set(this, JSON.parse(params.stores.getProperty(this.#key + '.index')!) as IndexProps);
+            return this;
+        }
         public setProps = () => {
-            const cached = caches.get(this);
-            if (!cached) return false;
-            params.stores.setProperty(this.#key + '.index', JSON.stringify(cached));
+            const cache = caches.get(this);
+            if (!cache) return false;
+            params.stores.setProperty(this.#key + '.index', JSON.stringify(cache));
             return true;
         }
         public indexCol = () => {
@@ -43,21 +46,46 @@ const Index = (() => {
             caches.set(this, props);
             return this;
         }
+        public deleteId = (type: 'unique' | 'double', id: string) => {
+            const cache = caches.get(this);
+            if (!cache) return false;
+            const found = type === 'unique' ? cache.unique.findIndex(elem => elem === id) : cache.double?.findIndex(elem => elem === id) as number;
+            if (found === -1) return false;
+            type === 'unique' ? cache.unique.splice(found, 1) : cache.double?.splice(found, 1);
+            caches.set(this, cache);
+            return true;
+        }
+        public addElems = (short: boolean, ...elems: string[] | [string, string | null][]) => {
+            const cache = caches.get(this);
+            if (!cache) return false;
+            if (short !== this.#info.short) return false;
+            const updatedIndex = short
+                ? [...(cache.unique as string[]), ...(elems as string[]).filter(elem => typeof elem === "string")]
+                : [...(cache.unique as [string, string | null][]), ...(elems as [string, string | null][]).filter(elem => Array.isArray(elem))];
+            const updatedUnique = short
+                ? Array.from(new Set(updatedIndex as string[]))
+                : Array.from(new Map(updatedIndex as [string, string | null][]));
+            caches.set(this, { ...cache, ...{ unique: updatedUnique } });
+            return true;
+        }
         public getInfo = () => ({...this.#info});
-        public setInfo = (params: Partial<ColumnInfo>) => this.#info = {...this.#info, ...params} as ColumnInfo;
+        public setInfo = (params: Partial<ColumnInfo>) => {
+            this.#info = {...this.#info, ...params};
+            return {... this.#info};
+        }
     }
 
     class Index {
         constructor(SS: GoogleAppsScript.Spreadsheet.Spreadsheet, stores?: GoogleAppsScript.Properties.Properties, prefix?: string) {
             this.setParams({SS: SS, stores: stores, prefix: prefix})
-            console.warn(params.SS.getName(), params.stores.getKeys(), prefix);
             keys = this.getKeys();
             this.initClass();
         }
         private initClass = () => {
             const storedInfo = params.stores.getProperty(params.prefix + '.info');
-            if (storedInfo) info = JSON.parse(storedInfo), keys.splice(keys.indexOf(params.prefix + '.info'), 1);
-            else info = {name: params.prefix, spreadsheet: params.SS.getId(), stores: keys.length};
+            if (storedInfo) info = JSON.parse(storedInfo);
+            else info = {name: params.prefix, spreadsheet: params.SS.getId()};
+            info.stores = keys.length;
             console.log(info);
         }
         private setParams = (args: Partial<typeof params>) => {
@@ -66,43 +94,39 @@ const Index = (() => {
             args.stores = args.stores ?? PropertiesService.getScriptProperties();
             args.prefix = args.prefix ?? labels(ssName);
             params = {...args} as typeof params;
+            console.warn(params.SS.getName(), params.stores.getKeys(), params.prefix);
         }
         public getInfo = () => ({...info});
-        public getKeys = () => params.stores.getKeys().filter(key => key.startsWith(params.prefix as string));
+        public getKeys = () => params.stores.getKeys().filter(this.isStore);
         public setInfo = (newInfo: Partial<IndexInfo>) => info = {...info, ...newInfo};
+        public isStore = (key: string) => key.startsWith(params.prefix) && !excludedStoreTags.some(tag => key.endsWith(tag));
         public getSheets = () => Array.from(sheets);
-        public writeCols = () => colMap.forEach((cols, sheet) => cols.forEach(col => object[sheet][col].setProps()));
-        public indexCols = () => colMap.forEach((cols, sheet) => cols.forEach(col => object[sheet][col].indexCol()));
-        public writeInfo = () => { 
-            info.stores = params.stores.getKeys().filter(key => key.startsWith(params.prefix)).length - 1;
-            params.stores.setProperty(params.prefix + '.info', JSON.stringify(info));
-        }
+        public writeCols = () => colMap.forEach((cols, sheet) => cols.forEach(col => objMod[sheet][col].setProps()));
+        public indexCols = () => colMap.forEach((cols, sheet) => cols.forEach(col => objMod[sheet][col].indexCol()));
+        public writeInfo = () => params.stores.setProperty(params.prefix + '.info', JSON.stringify({...info, ...{stores: this.getKeys().length}}));
         public getObjMod = () => {
             keys = this.getKeys();
             for (const key of keys) {
-                const sheet = key.split('.')[sheetAddress];
-                object[sheet] = {};
-                sheets.add(sheet);
+                const sheetName = key.split('.')[sheetAddress];
+                objMod[sheetName] = {};
+                sheets.add(sheetName);
             }
             for (const sheet of sheets) {
-                const columns = Array
-                    .from(new Set(keys.filter(key => key.startsWith(`${params.prefix}.${sheet}`)).map(key => key.split('.')[colAddress])));
+                const columns = keys.filter(key => key.startsWith(`${params.prefix}.${sheet}`)).map(key => key.split('.')[colAddress]);
                 colMap.set(sheet, columns);
-                for (const column of columns) object[sheet][column] = new ColumnIndex(`${params.prefix}.${sheet}.${column}`);
+                for (const column of columns) objMod[sheet][column] = new ColumnIndex(`${params.prefix}.${sheet}.${column}`);
             }
-            console.log(JSON.stringify(object, undefined, 1));
-            return {...object}
+            console.log(JSON.stringify(objMod, undefined, 1));
+            return {...objMod}
         };
         public addNewCol = (colInfo: ColumnInfo, colName?: string) => {
             const {A1, sheetName} = colInfo, sheet = params.SS.getSheetByName(sheetName);
-            if (!sheet) 
-                return `⛔ Found no sheet named "${sheetName}" in Spreadsheet "${params.SS.getName()}".`;
+            if (!sheet) return `⛔ Found no sheet named "${sheetName}" in Spreadsheet "${params.SS.getName()}".`;
             if (!colName) colName = labels(sheet.getRange(A1.charAt(0) + 1).getValue());
             const existing = colMap.get(sheetName);
-            if (existing && existing.includes(colName)) 
-                return `⛔ Existing index for "${colName}". Use .getObjMod().${sheetName}.${colName} to get it.`;
+            if (existing && existing.includes(colName)) return `⛔ "${colName}" already indexed. Use .getObjMod().${sheetName}.${colName} to interact.`;
             const colObject = new ColumnIndex(`${params.prefix}.${sheetName}.${colName}`, colInfo);
-            object[sheetName][colName] = colObject;
+            objMod[sheetName][colName] = colObject;
             existing ? colMap.set(sheetName, existing.concat(colName)) : colMap.set(sheetName, [colName]);
             console.log(`✅ Added index for "${colName}". Use .getObjMod().${sheetName}.${colName} to get it.`);
             return colObject;
@@ -114,13 +138,10 @@ const Index = (() => {
 function indexTests() {
     const Props = PropertiesService.getScriptProperties();
     const SS = SpreadsheetApp.openById(getFWDBLeads());
-    const Info : IndexInfo = {name: 'FWDBLeads', spreadsheet: SS.getName()};
-    Props.setProperty('FWDBLeads.info', JSON.stringify(Info));
-    const ColInfo : ColumnInfo = {A1: 'E', sheetName: 'LeadsDB', short: true};
-    Props.setProperty('FWDBLeads.LeadsDB.Jobs', JSON.stringify(ColInfo));
     const index = new Index(SS, Props);
     const FWDB = index.getObjMod();
     console.log('Jobs index:', FWDB.LeadsDB.Jobs.readProp());
+    console.log('Info object:', FWDB.info)
 }
 
 // FAST Indexing and double counting function: 1 and a half seconds for 700+ RichTextValue links!
